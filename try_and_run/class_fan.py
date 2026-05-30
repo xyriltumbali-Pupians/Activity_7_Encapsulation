deac9f47ebf91bc66d89b31988cf9b1ce7767929
xyriltumbali-Pupians <xyriltumbali@gmail.com>
2026-05-31 00:09:46 +0800

    continues progress

--- try_and_run/class_fan.py
+++ try_and_run/class_fan.py
@@ -1,14 +1,22 @@
 class Fan:
-    
+
     SLOW = 1
     MEDIUM = 2
     FAST = 3
 
     def __init__(self):
+        self.__speed = Fan.SLOW
+        self.__on = False
+        self.__raduis = 5      
+        self.__color = "blue"
 
     def display(self):
-        print("Speed:", self._speed)
-        print("Radius:", self._radius)
-        print("Color:", self._color)
-        print("On:", self._on)
+        print("Speed:", self.__speed)
+        print("Radius:", self.__radius)   
+        print("Color:", self.__color)
+        print("On:", self.__on)
         print()
+
+
+fan1 = Fan()
+fan1.display()
