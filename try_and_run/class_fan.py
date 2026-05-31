1a7ccf88f9e0e75fc8acad96ab400359ee849f56
xyriltumbali-Pupians <xyriltumbali@gmail.com>
2026-05-31 13:13:38 +0800

    working code

--- try_and_run/class_fan.py
+++ try_and_run/class_fan.py
@@ -10,6 +10,19 @@ class Fan:
         self.__radius = 5
         self.__color = "blue"
 
+    
+    def set_speed(self, speed):
+        self.__speed = speed
+
+    def set_radius(self, radius):
+        self.__radius = radius
+
+    def set_color(self, color):
+        self.__color = color
+
+    def set_on(self, on):
+        self.__on = on
+
     def display(self):
         print("Speed:", self.__speed)
         print("Radius:", self.__radius)
@@ -17,6 +30,17 @@ class Fan:
         print("On:", self.__on)
         print()
 
-
 fan1 = Fan()
+fan1.set_speed(Fan.FAST)
+fan1.set_radius(10)
+fan1.set_color("yellow")
+fan1.set_on(True)
+
+fan2 = Fan()
+fan2.set_speed(Fan.MEDIUM)
+fan2.set_radius(5)
+fan2.set_color("blue")
+fan2.set_on(False)
+
 fan1.display()
+fan2.display()
