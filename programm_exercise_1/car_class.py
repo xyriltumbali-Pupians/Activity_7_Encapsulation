b2d9c5108bbaf469e29205efe4859eb2878d9a56
xyriltumbali-Pupians <xyriltumbali@gmail.com>
2026-05-31 21:14:52 +0800

    wrong indentation error resolved

--- programm_exercise_1/car_class.py
+++ programm_exercise_1/car_class.py
@@ -3,10 +3,10 @@ class Car:
     def __init__(self, year_model, make):
         self.year_model = year_model
         self.make = make
-        self.speed = 0
+        self.__speed = 0
 
     def accelerate(self):
-       self.__speed += 5
+        self.__speed += 5
 
     def brake(self):
         self.__speed -= 5 
